1ec12126d42187eb0e4a77d2bd609e73592d4084
Rominet13 <volphoenix@outlook.fr>
2016-11-02 21:32:59 +0100

    ajout de l'enum pour contenir les articles avec trous à remplir

---
+++ src/main/java/fr/cntcnc/migration/html4joomla1to3/ArticlesViergesEnum.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.cntcnc.migration.html4joomla1to3;
+
+/**
+ *
+ * @author volph
+ */
+public enum ArticlesViergesEnum {
+    SANSFLV("<table><td style=\"padding-left: 30px;\">\n"
+            + "<a data-rokbox data-rokbox-album=\"clubsHD\" href=\"%s\"><img src=\"%s\" /></a>\n"
+            + "<br>\n"
+            + "<center><a data-rokbox data-rokbox-album=\"clubsSD\" href=\"%s\"> Qualité standard </a></center>\n"
+            + "</td>\n"
+            + "<td style=\"padding-left: 30px;\"> \n"
+            + "  <b><big>   \n"
+            + "    %s \n"
+            + "   </big> </b>\n"
+            + "</td></table>\n\n"),
+    AVECFLV("<table><td style=\"padding-left: 30px;\">\n"
+            + "   <video id=\"video1\" class=\"video-js vjs-default-skin\" width=\"640\" height=\"480\"\n"
+            + "        data-setup='{\"controls\" : true, \"autoplay\" : false, \"preload\" : \"auto\"}' poster=\"%s\">\n"
+            + "        <source src=\"%s\" type=\"video/x-flv\">\n"
+            + "    </video>\n"
+            + "<br>\n"
+            + "<center><a data-rokbox data-rokbox-album=\"clubsSD\" href=\"%s\"> Visionner en <img src=\"media/CNT/images/icones/HD_Small.png\" border=\"0\" /> </a></center>\n"
+            + "</td>\n"
+            + "<td style=\"padding-left: 30px;\"> \n"
+            + "  <b><big>   \n"
+            + "    %s \n"
+            + "   </big> </b>\n"
+            + "</td></table>\n\n");
+
+    private String nvArticleVierge;
+
+    private ArticlesViergesEnum(String nvArticleVierge) {
+        this.nvArticleVierge = nvArticleVierge;
+    }
+
+    public String getNvArticleVierge() {
+        return nvArticleVierge;
+    }
+
+}
